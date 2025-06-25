bf9d8b3cca41f6c7cf19e4fbcdac328710557d70
CUPIDUST <sandipbehera681@gmail.com>
2025-06-25 16:25:01 +0530

    Implemented searching funcionality in exams

--- src/components/examStack/ExamPage.jsx
+++ src/components/examStack/ExamPage.jsx
@@ -9,7 +9,7 @@ import {
 } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import blankArrayImage from "../../images/emptyArrayIcon.png";
-import { useCallback, useContext, useEffect, useState } from "react";
+import { useCallback, useContext, useEffect, useRef, useState } from "react";
 import { MMKVLoader } from "react-native-mmkv-storage";
 import ShowExamAndSubjects from "../etc/ShowExamAndSubjects";
 import { useFocusEffect } from "@react-navigation/native";
@@ -21,13 +21,39 @@ const ExamPage = ({ navigation }) => {
   const isDarkMode = useColorScheme() === "dark";
   const [inputBoxValue, setInputBoxValue] = useState("");
   const [examArray, setExamArray] = useState([]);
+  const [originalExamArray, setOriginalExamArray] = useState([]);
 
   const styles = getStyles(isDarkMode);
+  const inputRef = useRef(null);
 
   const { selectedIndexes, setLongPressed } = useContext(LongPressedContext);
 
   const getExamList = () => {
     setExamArray(MMKV.getMap("examArray"));
+    setOriginalExamArray(MMKV.getMap("examArray"));
+  };
+
+  const handleSearch = (e) => {
+    setInputBoxValue(e);
+    setExamArray(
+      // console.log(
+      originalExamArray.filter(
+        (originalExam) =>
+          // console.log(
+          originalExam.examName.toLowerCase().includes(String(e).toLowerCase())
+        // );
+        // console.log(String(e).toLowerCase());
+      )
+    );
+    // );
+  };
+
+  const removeFocus = () => {
+    if (inputRef.current) {
+      inputRef.current.blur();
+    }
+    setInputBoxValue("");
+    setExamArray(originalExamArray);
   };
 
   useFocusEffect(
@@ -56,17 +82,32 @@ const ExamPage = ({ navigation }) => {
       style={[styles.container]}
       onPress={() => {
         setLongPressed(false);
+        removeFocus();
       }}
     >
       {examArray && (
-        <TextInput
-          placeholder="🔍 Search"
-          keyboardType="default"
-          style={[styles.searchBox]}
-          value={inputBoxValue}
-          onChange={setInputBoxValue}
-          placeholderTextColor={isDarkMode ? "white" : "black"}
-        />
+        <View style={[styles.searchBoxAndIconContainer]}>
+          <TextInput
+            ref={inputRef}
+            placeholder="🔍 Search"
+            keyboardType="default"
+            style={[
+              styles.searchBox,
+              !inputBoxValue.length > 0 ? styles.additionalInputBoxStyle : null,
+            ]}
+            value={inputBoxValue}
+            onChangeText={(e) => {
+              handleSearch(e);
+            }}
+            placeholderTextColor={isDarkMode ? "white" : "black"}
+          />
+          {inputBoxValue.length > 0 && (
+            <Pressable style={[styles.cancelIcon]} onPress={removeFocus}>
+              {/* <Icon name="close-circle-outline" size={28} color="blue" /> */}
+              <Icon name="close" size={28} color="white" />
+            </Pressable>
+          )}
+        </View>
       )}
       <View style={[styles.showExamNameContainer]}>
         {!Array.isArray(examArray) || examArray?.length === 0 ? (
@@ -128,11 +169,30 @@ const getStyles = (isDarkMode) =>
       right: 25,
     },
     searchBox: {
-      marginVertical: 8,
-      marginHorizontal: 15,
       paddingHorizontal: 18,
-      borderRadius: 15,
       backgroundColor: isDarkMode ? "#23282c" : "#f6f5f3",
+      borderTopLeftRadius: 15,
+      borderBottomLeftRadius: 15,
+      color: isDarkMode ? "white" : "black",
+      flex: 1,
     },
     noExamText: { color: isDarkMode ? "white" : "black", fontSize: 16 },
+    searchBoxAndIconContainer: {
+      marginVertical: 8,
+      marginHorizontal: 15,
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+    },
+    cancelIcon: {
+      backgroundColor: "red",
+      borderTopRightRadius: 15,
+      borderBottomRightRadius: 15,
+      paddingHorizontal: 8,
+      paddingVertical: 6,
+    },
+    additionalInputBoxStyle: {
+      borderTopRightRadius: 15,
+      borderBottomRightRadius: 15,
+    },
   });
